fac6a9471f3d3e31e0228911beb95245a6ecd588
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2025-11-08 13:47:31 -0500

    Start booster animation

--- Scripts/Kart/HoverBooster.cs
+++ Scripts/Kart/HoverBooster.cs
@@ -3,7 +3,19 @@ using System;
 
 public partial class HoverBooster : Node3D
 {
+    [ExportCategory("Functional")]
     [Export] public bool IsFront;
     [Export] public RayCast3D Ray;
     [Export] public MeshInstance3D Mesh;
+    
+    [ExportCategory("Visual")]
+    [Export] public float DistanceBobbingSpeed = 2f;
+    [Export] public float NormalRotationSpeed = 2f;
+    [Export] public float DistanceBobbingMax = 0.25f;
+    [Export] public float NormalRotationAngleMax = 45f; // In degrees
+
+    public override void _Process(double delta)
+    {
+        
+    }
 }
